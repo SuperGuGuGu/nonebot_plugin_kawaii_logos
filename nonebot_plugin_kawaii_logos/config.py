03bc7298680f78186c393bf4d4df1bd45e661cb4
SuperGuGuGu <13680478000@163.com>
2025-01-15 21:50:50 +0800

    feat: update了

--- nonebot_plugin_kawaii_logos/config.py
+++ nonebot_plugin_kawaii_logos/config.py
@@ -3,17 +3,20 @@ from pydantic import BaseModel
 from pathlib import Path
 from nonebot import require
 
-require("nonebot_plugin_localstore")
+try:
+    require("nonebot_plugin_localstore")
+    import nonebot_plugin_localstore as store
 
-import nonebot_plugin_localstore as store
+    plugin_cache_dir: Path = store.get_plugin_cache_dir()
+    # plugin_config_dir: Path = store.get_plugin_config_dir()
+    plugin_data_dir: Path = store.get_plugin_data_dir()
+except Exception as e:
+    plugin_cache_dir: Path = Path("./nonebot_plugin_kawaii_logos/cache")
+    plugin_data_dir: Path = Path("./nonebot_plugin_kawaii_logos/data")
 
 
 class Config(BaseModel):
-    qbm_url: str
-    qbm_username: str
-    qbm_password: str
-    qbm_enable_user: list[str] = []
-    qbm_send_text: bool = False
+    ...
 
 
 menu_data = [
@@ -27,7 +30,3 @@ menu_data = [
 
 plugin_config = get_plugin_config(Config)
 # qb_url = plugin_config.qbm_url
-
-plugin_cache_dir: Path = store.get_plugin_cache_dir()
-# plugin_config_dir: Path = store.get_plugin_config_dir()
-plugin_data_dir: Path = store.get_plugin_data_dir()
