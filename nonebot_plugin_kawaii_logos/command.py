b301e6188eeb80909c46e5cdc4263665e4f05879
SuperGuGuGu <13680478000@163.com>
2025-01-16 17:32:37 +0800

    feat: 识别输入的文字

--- nonebot_plugin_kawaii_logos/command.py
+++ nonebot_plugin_kawaii_logos/command.py
@@ -5,12 +5,20 @@ from .draw import template_1
 
 async def command_kawaii_logos(args: str):
     # 解析
-    logo_data: dict = {
-        "标题": "あアいイうウ",
-        "副标题": "あアいイう",
-        "下": "text",
-        "上": "text",
-    }
+    args = args.split(" ")
+    if len(args) == 0:
+        return "请添加要生成logo的文字"
+    elif len(args) == 1:
+        logo_data: dict = {"标题": args[0], "副标题": None, "下": None,"上": None}
+    elif len(args) == 2:
+        logo_data: dict = {"标题": args[0], "副标题": args[1], "下": None,"上": None}
+    elif len(args) == 3:
+        logo_data: dict = {"标题": args[0], "副标题": args[1], "下": args[2],"上": None}
+    elif len(args) == 4:
+        logo_data: dict = {"标题": args[0], "副标题": args[1], "下": args[2],"上": args[3]}
+    else:
+        return "文字参数过多，最多包含4段文字"
+
     # 检查内容长度
     if not 3 <= len(logo_data["标题"]):
         return "第1段文字过短"
